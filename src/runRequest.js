eba0caa467918809214f537cb4fa33cca5bd309c
koji <robson.koji@gmail.com>
2021-11-28 18:43:51 -0300

    search and chart customization.

--- src/runRequest.js
+++ src/runRequest.js
@@ -1,5 +1,5 @@
 export default async function runRequest(body) {
-  // const response = await fetch("http://shedar.fapesp.br:5000/.netlify/functions/search", {
+  // const response = await fetch("http://<server>/.netlify/functions/search", {
   const response = await fetch(process.env.REACT_APP_API_URL + ".netlify/functions/search", {
     method: "POST",
     headers: { "content-type": "application/json" },
